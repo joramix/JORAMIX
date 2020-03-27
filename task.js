69956b8f628b71245624a236d72cf733b7ff621f
joramix <61960966+joramix@users.noreply.github.com>
2020-03-27 07:54:05 +0100

    Add files via upload

---
+++ task.js
@@ -0,0 +1,28 @@
+var sn ="<b>Full Name: </b>"
+var sn1 =" Joram Benjamin"
+document.write(sn+sn1);
+
+document.write("<br>")
+document.write("<br>")
+
+let sn5=["<b>FRONTEND COURSES;</b>","1.HTML", "2.CSS", "3.JS"];
+document.write(sn5[0])
+document.write("<br>")
+document.write(sn5[1])
+document.write("<br>")
+document.write(sn5[2])
+document.write("<br>")
+document.write(sn5[3])
+document.write("<br> <br>")
+
+var noct="<b>Number Of Courses Taken= </b>"
+var noct1="3(odd number)"
+var noct2="<i><b>Display of Odd number from 1-200;</b></i>"
+document.write(noct+noct1);
+document.write("<br>")
+document.write(noct2)
+document.write("<br>")
+
+ for (i=1; i<=200; i+=2) {
+    document.write(i + "<br />");
+    }
